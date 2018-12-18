d153dd5b6813707970457abfe97c64202464c942
Mo Sharif <mo@mosh-media.com>
2018-12-18 10:12:29 -0800

    Cleaned landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -10,11 +10,6 @@ import { GitHubIcon } from 'rmw-shell/lib/components/Icons'
 import { Helmet } from 'react-helmet'
 import { withRouter } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import classNames from 'classnames'
-import CardMedia from '@material-ui/core/CardMedia'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 
@@ -23,7 +18,7 @@ const styles = theme => ({
     position: 'relative',
   },
   textColor: {
-    color: 'white',
+    color: 'black',
   },
   icon: {
     marginRight: theme.spacing.unit * 2,
@@ -42,6 +37,10 @@ const styles = theme => ({
     maxWidth: 600,
     margin: '0 auto',
     padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+    'text-align': 'center',
+    backgroundColor: 'white',
+    'border-radius': 12,
+    'box-shadow': '0px 16px 20px rgba(0, 0, 0, 0.45)'
   },
   heroButtons: {
     marginTop: theme.spacing.unit * 4,
@@ -75,8 +74,6 @@ const styles = theme => ({
     padding: theme.spacing.unit * 6,
   },
 })
-
-const cards = [3]
 
 class LandingPage extends Component {
 
@@ -152,12 +149,16 @@ class LandingPage extends Component {
             {/* Hero unit */}
             <div className={classes.heroUnit} >
               <div className={classes.heroContent}>
+               <img src="https://cdn.pbrd.co/images/HSkE2Wm.png" 
+                  alt="Promania" 
+                  width="270"
+               />
                 <Typography 
                   component="h2" variant="h2" align="center" className={classes.textColor} gutterBottom>
                   Promania
                 </Typography>
                 <Typography 
-                  variant="h6" align="center"  className={classes.textColor} color="textPrimary" paragraph>
+                  variant="h6" align="center"  className={classes.textColor} color="white" paragraph>
                   Launch your dashboard quickly without the headache
                 </Typography>
                 <div className={classes.heroButtons}>
@@ -172,38 +173,6 @@ class LandingPage extends Component {
                   </Grid>
                 </div>
               </div>
-            </div>
-            <div className={classNames(classes.layout, classes.cardGrid)}>
-              {/* End hero unit */}
-              <Grid container spacing={40}>
-                {cards.map(card => (
-                  <Grid item key={card} sm={6} md={4} lg={3}>
-                    <Card className={classes.card}>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                        title="Image title"
-                      />
-                      <CardContent className={classes.cardContent}>
-                        <Typography  variant="h5" component="h2">
-                          Heading
-                        </Typography>
-                        <Typography>
-                          This is a media card. You can use this section to describe the content.
-                        </Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small" color="primary">
-                          View
-                        </Button>
-                        <Button size="small" color="primary">
-                          Edit
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
             </div>
           </main>
           {/* Footer */}
